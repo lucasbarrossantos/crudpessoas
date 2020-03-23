efdf846c2970efb088c9b56ed254e7950104edb9
Lucas Barros Santos <lucas-barros28@hotmail.com>
2020-03-23 02:01:46 -0300

    Configuração de Profiles do Spring Boot para dev e prod

--- src/main/java/com/crudpessoas/core/heroku/DatabaseConfig.java
+++ src/main/java/com/crudpessoas/core/heroku/DatabaseConfig.java
@@ -1,20 +1,38 @@
 package com.crudpessoas.core.heroku;
 
 import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
+@Getter
+@Setter
 @Configuration
+@ConfigurationProperties("spring.datasource")
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
+    private String url;
+    private String username;
+    private String password;
 
+    @Profile("dev")
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSourceDev() {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
         return new HikariDataSource(config);
     }
+
+    @Profile("prod")
+    @Bean
+    public DataSource dataSourceProd() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        return new HikariDataSource(config);
+    }
+
 }
